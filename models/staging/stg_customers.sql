2626bb0a6f8037856ce98a501b4e49f4d9953ba6
niltoncurso01@gmail.com <niltoncurso01@gmail.com>
2022-05-17 00:39:00 +0000

    Creating table customers and test run dbt

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,24 @@
+with renamed as (
+    select
+        customerid
+        , firstname
+        , middlename
+        , lastname
+        , case
+            when title = 'Mr.'
+                then 'H'
+            when title = 'Ms.'
+                then 'M'
+            else 'Fora do padrão'
+        end persons_sex
+        , suffix
+        , companyname
+        , salesperson
+        , emailaddress
+        , phone
+    from adventure_works_etl.public_customer
+)
+
+select *
+from renamed
+
